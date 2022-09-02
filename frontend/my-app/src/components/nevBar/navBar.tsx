e225c9e00151d135672f1f596aad779c7f4c0c09
Onesco <Okonkwoonesmus5@gmail.com>
2022-09-02 23:11:46 +0100

    [setup] nav-bar setup

--- frontend/my-app/src/components/nevBar/navBar.tsx
+++ frontend/my-app/src/components/nevBar/navBar.tsx
@@ -1,21 +1,30 @@
 import './navBar.css'
+import { useState } from 'react'
+
 
 export default function NavBar() {
+ const [tuggle, setTuggle] = useState(false)
+ 
   return (
     <div className='navbar-wrapper'>
       <div className="navbar">
-      <div className='left-nav'>
-        <i>Icon</i> <span className='logo-title'>Santos Clothing</span>
+        <div className='left-nav'>
+          <i>Icon</i> <span className='logo-title'>Santos Clothing</span>
+        </div>
+        <div className={tuggle?'right-nav display':'right-nav hidden'}>
+          <div className='right-nav-item'>Home</div>
+          <div className='right-nav-item'>Category</div>
+          <div className='right-nav-item'>Promo</div>
+          <div className='right-nav-item'>About</div>
+          <button> Connect me</button>
+        </div>
+        <button 
+          className='hambuger'
+          onClick={()=>{setTuggle((prevState)=>!prevState)}}
+        >
+         {tuggle?"close":"open"}
+        </button>
       </div>
-      <div className='right-nav'>
-        <div className='right-nav-itme'>Home</div>
-        <div className='right-nav-itme'>Category</div>
-        <div className='right-nav-itme'>Promo</div>
-        <div className='right-nav-itme'>About</div>
-        <button> Connect me</button>
-      </div>
-      <div className='hambuger'>Menu</div>
-    </div>
     </div>
   )
 }

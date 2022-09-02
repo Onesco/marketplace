e474e12288573911891a638490f4777de0ee1a6b
Onesco <Okonkwoonesmus5@gmail.com>
2022-09-02 23:09:51 +0100

    banner component set up

--- frontend/my-app/src/components/banner/Banner.tsx
+++ frontend/my-app/src/components/banner/Banner.tsx
@@ -1,11 +1,11 @@
 import './Banner.css'
+import banner  from '../../images/banner-1.jpg'
 
 
 export default function Banner() {
   return (
-    <div>
-      <img width={30} src='https://pngimg.com/uploads/apple/apple_PNG12400.png' alt='hands'/>
+    <div className='banner'>
+      <img className='banner-image' src={banner} alt='hands'/>
     </div>
   )
 }
- 

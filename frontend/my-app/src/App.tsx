d0ed07050f3ed3293d0ebf17265d9ca23cbb1611
Onesco <Okonkwoonesmus5@gmail.com>
2022-09-02 23:08:57 +0100

    update to the app file

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import NavBar from './components/nevBar/navBar';
 import Banner from './components/banner/Banner';
+import Home from './pages/home';
+
 function App() {
   return (
     <div>
       <NavBar />
       <Banner/>
-      App
+      <Home/>
     </div>
   );
 }

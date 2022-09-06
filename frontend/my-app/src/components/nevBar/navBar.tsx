8bfbf237c07cb302d9af2dc931ae24800e0ed5dc
Onesco <Okonkwoonesmus5@gmail.com>
2022-09-06 16:51:33 +0100

    update the  navbar components

--- frontend/my-app/src/components/nevBar/navBar.tsx
+++ frontend/my-app/src/components/nevBar/navBar.tsx
@@ -2,6 +2,33 @@ import './navBar.css'
 import { useState } from 'react'
 
 
+const style ={
+ fill:"black"
+} as React.CSSProperties
+
+const CloseIcon = ()=>{
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" style={style}>
+      <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031
+        L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 
+        3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z">
+      </path>
+    </svg>
+  )
+}
+
+const OpenIcon = ()=>{
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" style={style}>
+      <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 
+      3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1
+      0 27 21 L 3 21 z">
+      </path>
+    </svg>
+  )
+}
+
+
 export default function NavBar() {
  const [tuggle, setTuggle] = useState(false)
  
@@ -9,21 +36,19 @@ export default function NavBar() {
     <div className='navbar-wrapper'>
       <div className="navbar">
         <div className='left-nav'>
-          <i>Icon</i> <span className='logo-title'>Santos Clothing</span>
+         <span className='logo-title'>Sanshop</span>
         </div>
         <div className={tuggle?'right-nav display':'right-nav hidden'}>
           <div className='right-nav-item'>Home</div>
           <div className='right-nav-item'>Category</div>
           <div className='right-nav-item'>Promo</div>
           <div className='right-nav-item'>About</div>
-          <button> Connect me</button>
+          <button>Sign Up</button>
         </div>
-        <button 
-          className='hambuger'
-          onClick={()=>{setTuggle((prevState)=>!prevState)}}
-        >
-         {tuggle?"close":"open"}
-        </button>
+        <div className='hambuger' onClick={()=>{setTuggle((prevState)=>!prevState)}}>
+         {tuggle?<CloseIcon/>:<OpenIcon/>}
+        </div>
+        
       </div>
     </div>
   )
--- frontend/my-app/src/pages/home/components/itemListing/index.tsx
+++ frontend/my-app/src/pages/home/components/itemListing/index.tsx
@@ -1,20 +1,18 @@
 import Item from '../items';
 
+
 interface IItemListing {
   position:string
 }
 
-const numItems =[
+const numItems :IItemListing[] =[
   {position:'one'},
   {position:'two'},
   {position:'three'},
   {position:'four'},
-  {position:''}
+  {position:'five'}
 ]
 
-const renderItems = (itemListing:IItemListing[]) => {
-  return itemListing.map( (eachItem) => <Item pos ={eachItem.position}/>)
-}
 
 export default function ItemListing () {
   return (
@@ -22,4 +20,8 @@ export default function ItemListing () {
       {renderItems(numItems)}
     </div>
   );
+}
+
+const renderItems = (itemListing:IItemListing[]) => {
+  return itemListing.map( (eachItem:IItemListing) => <Item pos ={eachItem.position}/>)
 }
